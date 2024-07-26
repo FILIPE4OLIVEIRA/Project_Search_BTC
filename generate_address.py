a393aa21adf40a2e32451a8a725b99fa1ecaf414
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2024-07-26 10:12:25 -0300

    Delete generate_address.py

--- generate_address.py
+++
@@ -1,7 +0,0 @@
-from bitcoinlib.keys import HDKey
-
-def generate_address(key):
-    private_key_hex = hex(key)[2:].zfill(64)
-    public_address = HDKey(private_key_hex).address()
-    wif_private_key = HDKey(private_key_hex).wif_key()
-    return public_address, wif_private_key
